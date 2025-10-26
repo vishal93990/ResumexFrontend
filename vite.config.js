fe78f6f7be71ef4051ed3545313cf621d7295ad7
Vishal Jha <vjha93011@gmail.com>
2025-10-26 19:29:33 +0530

    feat: Integrate Tailwind CSS plugin into Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: "./",
   build: {
     outDir: "dist",
-    sourcemap: false, 
-  },
-  server: {
-    port: 3000,
   },
 });
